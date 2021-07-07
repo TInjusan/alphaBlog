c73be4742a2a36d8a4b0b3a2d5a922589987c8d9
TInjusan <jaztyn72@gmail.com>
2021-07-07 22:25:24 +0800

    Make app production ready

---
+++ db/migrate/20210707140849_add_title_and_description_to__articles.rb
@@ -0,0 +1,6 @@
+class AddTitleAndDescriptionToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :title, :string
+    add_column :articles, :description, :text
+  end
+end
